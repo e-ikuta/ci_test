69d9578eb5d9ade8fcf31594b4f1a0735818b42c
e_ikuta <e_ikuta@ga-tech.co.jp>
2022-10-17 13:27:25 +0900

    かつブランチがmain

--- .circleci/set_env.sh
+++ .circleci/set_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ ${CIRCLE_TAG} =~ ^release_[0-9]+$ ]]
+if [[ ${CIRCLE_TAG} =~ ^release_[0-9]+$ ]] && [ ${CIRCLE_BRANCH} = main ]
 then
   echo deploying with tag push
 else
